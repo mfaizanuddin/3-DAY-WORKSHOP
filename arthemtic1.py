b431c234e85fe1ec9f43ac7cbda669797a6d4959
mfaizanuddin <mohdfaizanuddin00@gmail.com>
2026-09-07 14:49:50 +0530

    Add files via upload

---
+++ arthemtic1.py
@@ -0,0 +1,8 @@
+a= int(input("Enter first number: "))
+b= int(input("Enter second number: "))
+
+print("sum =",a + b)
+print("Difference =",a - b)
+print("product =",a * b) 
+print("Quotient =",a / b)
+print("Reminder =",a % b)
---
+++ arthmetic2.py
@@ -0,0 +1,3 @@
+A = int(input("Enter the radius of the circle: "))
+area = 3.14 * A * A
+print("The area of the circle is:", area)
