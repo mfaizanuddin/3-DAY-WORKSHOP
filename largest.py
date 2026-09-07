4c24f9e2c7797b34d1b4b3a747747cb49327fbbd
mfaizanuddin <mohdfaizanuddin00@gmail.com>
2026-09-08 00:58:01 +0530

    Add files via upload

---
+++ largest.py
@@ -0,0 +1,6 @@
+A = int(input("Enter a number: "))
+B = int(input("Enter another number: "))
+if A > B:
+    print("The largest number is first one :", A)
+else:
+    print("The largest number is second one :", B)
