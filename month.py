455d7d8564b9f870bc72c3abb6369ddf5844202d
mfaizanuddin <mohdfaizanuddin00@gmail.com>
2026-09-08 12:01:56 +0530

    Add files via upload

---
+++ month.py
@@ -0,0 +1,8 @@
+month = int(input("Enter month: "))
+
+if month == 2:
+    print("28 days")
+elif month == 4 or month == 6 or month == 9 or month == 11:
+    print("30 days")
+else:
+    print("31 days")
