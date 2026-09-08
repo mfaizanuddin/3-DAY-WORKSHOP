edab46836cc1bdc80f1115a4e487687e142e59b1
mfaizanuddin <mohdfaizanuddin00@gmail.com>
2026-09-08 10:50:38 +0530

    Add files via upload

---
+++ marks.py
@@ -0,0 +1,9 @@
+A = int(input("Enter the marks of the student: "))
+if A>=90 and A<=100:
+    print("The grade is A")
+elif A>=80 and A<90:
+    print("The grade is B")
+elif A < 80 and A>=70:
+    print("The grade is C")
+else:
+    print("The grade is D")
