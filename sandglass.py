f31cfb895cfff7cefe233ac9c0ae387d8930f0bb
mfaizanuddin <mohdfaizanuddin00@gmail.com>
2026-09-08 21:36:33 +0700

    Add files via upload

---
+++ sandglass.py
@@ -0,0 +1,19 @@
+n = int(input("Enter number of rows: "))
+
+for i in range(n, 0, -1):
+    for j in range(n - i):
+        print(" ", end="")
+
+    for j in range(2 * i - 1):
+        print("*", end="")
+
+    print()
+
+for i in range(2, n + 1):
+    for j in range(n - i):
+        print(" ", end="")
+
+    for j in range(2 * i - 1):
+        print("*", end="")
+
+    print()
