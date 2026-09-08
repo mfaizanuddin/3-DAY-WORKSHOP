8e0af1a14f0039934e90b0cee8a5e9a8d838a225
mfaizanuddin <mohdfaizanuddin00@gmail.com>
2026-09-08 11:07:16 +0530

    Add files via upload

---
+++ week.py
@@ -0,0 +1,15 @@
+A = int(input("Enter  week number: "))
+if A == 1:
+    print("Monday")
+elif A == 2:
+    print("Tuesday")
+elif A == 3:
+    print("Wednesday")
+elif A == 4:
+    print("Thursday")
+elif A == 5:
+    print("Friday")
+elif A == 6:
+    print("Saturday")
+else:
+    print("Sunday")
